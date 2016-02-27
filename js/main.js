74c2a56338bc500996c4f2d97576a88415a3f1be
Adrien Bertrand <bertrand.adrien@gmail.com>
2016-02-27 03:04:11 -0500

    Added Object.watch polyfill

--- js/main.js
+++ js/main.js
@@ -1,3 +1,60 @@
+/*
+ * object.watch polyfill from https://gist.github.com/eligrey/384583
+ *
+ * 2012-04-03
+ *
+ * By Eli Grey, http://eligrey.com
+ * Public Domain.
+ * NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
+ */
+
+// object.watch
+if (!Object.prototype.watch) {
+	Object.defineProperty(Object.prototype, "watch", {
+		enumerable: false
+		, configurable: true
+		, writable: false
+		, value: function (prop, handler) {
+			var
+					oldval = this[prop]
+					, newval = oldval
+					, getter = function () {
+						return newval;
+					}
+					, setter = function (val) {
+						oldval = newval;
+						return newval = handler.call(this, prop, oldval, val);
+					}
+					;
+
+			if (delete this[prop]) { // can't watch constants
+				Object.defineProperty(this, prop, {
+					get: getter
+					, set: setter
+					, enumerable: true
+					, configurable: true
+				});
+			}
+		}
+	});
+}
+
+// object.unwatch
+if (!Object.prototype.unwatch) {
+	Object.defineProperty(Object.prototype, "unwatch", {
+		enumerable: false
+		, configurable: true
+		, writable: false
+		, value: function (prop) {
+			var val = this[prop];
+			delete this[prop]; // remove accessors
+			this[prop] = val;
+		}
+	});
+}
+
+
+
 angular.module('openspire-editor', ['ngAnimate', 'ui.bootstrap']);
 
 angular.module('openspire-editor').controller('navbarctrl', function ($scope) {	
